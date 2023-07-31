8f2fca4fcf5a7da9db82d7f0dfca4aead1bbc07c
roncewind <dad@lynntribe.net>
2023-07-31 15:23:32 -0700

    #13 save point

---
+++ queues/rabbitmq/rabbitmq.go
@@ -0,0 +1,434 @@
+package rabbitmq
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/url"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/roncewind/go-util/queues"
+	"github.com/roncewind/go-util/util"
+)
+
+type Client struct {
+	ExchangeName string
+	QueueName    string
+	// desired / default delay durations
+	ReconnectDelay time.Duration
+	ReInitDelay    time.Duration
+	ResendDelay    time.Duration
+	RoutingKey     string
+
+	connection      *amqp.Connection
+	channel         *amqp.Channel
+	done            chan bool
+	isReady         bool
+	logger          *log.Logger
+	notifyConnClose chan *amqp.Error
+	notifyChanClose chan *amqp.Error
+	notifyConfirm   chan amqp.Confirmation
+	notifyReady     chan interface{}
+	// current delay durations
+	reconnectDelay time.Duration
+	reInitDelay    time.Duration
+	resendDelay    time.Duration
+}
+
+type RabbitError struct {
+	error
+}
+
+var (
+	errAlreadyClosed = RabbitError{util.WrapError(nil, "already closed: not connected to the server")}
+	errShutdown      = RabbitError{util.WrapError(nil, "client is shutting down")}
+	// errAlreadyClosed = errors.New("already closed: not connected to the server")
+	// errShutdown      = errors.New("client is shutting down")
+)
+
+// ----------------------------------------------------------------------------
+
+// New creates a single RabbitMQ client that will automatically
+// attempt to connect to the server.  Reconnection delays are set to defaults.
+func NewClient(urlString string) (*Client, error) {
+
+	u, err := url.Parse(urlString)
+	if err != nil {
+		return nil, RabbitError{util.WrapError(err, "unable to parse RabbitMQ URL string")}
+	}
+	// seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	queryMap, _ := url.ParseQuery(u.RawQuery)
+	if len(queryMap["exchange"]) < 1 || len(queryMap["queue-name"]) < 1 {
+		return nil, RabbitError{util.WrapError(err, "please define an exchange and queue-name as query parameters")}
+		// panic("Please define an exchange and queue-name as query parameters.")
+	}
+	routingKey := queryMap["queue-name"][0]
+	if len(queryMap["routing-key"]) > 0 {
+		routingKey = queryMap["routing-key"][0]
+	}
+
+	client := Client{
+		ExchangeName:   queryMap["exchange"][0],
+		QueueName:      queryMap["queue-name"][0],
+		ReconnectDelay: 2 * time.Second,
+		ReInitDelay:    2 * time.Second,
+		ResendDelay:    1 * time.Second,
+		RoutingKey:     routingKey,
+
+		done:        make(chan bool),
+		logger:      log.New(os.Stdout, "", log.LstdFlags),
+		notifyReady: make(chan interface{}),
+	}
+	client.reconnectDelay = client.ReconnectDelay
+	client.reInitDelay = client.ReInitDelay
+	client.resendDelay = client.ResendDelay
+	go client.handleReconnect(urlString)
+	return &client, nil
+}
+
+// ----------------------------------------------------------------------------
+
+// Init initializes a single RabbitMQ client that will automatically
+// attempt to connect to the server.
+func Init(client *Client, urlString string) *Client {
+
+	// set up defaults if none provided
+	if client.ReconnectDelay <= 0 {
+		client.ReconnectDelay = 2 * time.Second
+	}
+	if client.ReInitDelay <= 0 {
+		client.ReInitDelay = 2 * time.Second
+	}
+	if client.ResendDelay <= 0 {
+		client.ResendDelay = 1 * time.Second
+	}
+
+	// set up internals
+	client.done = make(chan bool)
+	client.logger = log.New(os.Stdout, "", log.LstdFlags)
+	client.notifyReady = make(chan interface{})
+
+	client.reconnectDelay = client.ReconnectDelay
+	client.reInitDelay = client.ReInitDelay
+	client.resendDelay = client.ResendDelay
+	go client.handleReconnect(urlString)
+	return client
+}
+
+// ----------------------------------------------------------------------------
+
+// handleReconnect will wait for a connection error on
+// notifyConnClose, and then continuously attempt to reconnect.
+func (client *Client) handleReconnect(addr string) {
+	for {
+		client.isReady = false
+		client.logger.Println("Attempting to connect")
+
+		conn, err := client.connect(addr)
+
+		if err != nil {
+			client.logger.Println("Connect error:", err)
+			client.logger.Println("Failed to connect. Retrying in", client.reconnectDelay)
+
+			select {
+			case <-client.done:
+				client.logger.Println("done.")
+				return
+			case <-time.After(client.reconnectDelay):
+				client.reconnectDelay = client.progressiveDelay(client.reconnectDelay)
+			}
+			client.logger.Println("continue.")
+			continue
+		}
+
+		// reset client.reconnectDealy
+		client.reconnectDelay = client.ReconnectDelay
+		if done := client.handleReInit(conn); done {
+			break
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------
+
+// connect will create a new AMQP connection
+func (client *Client) connect(addr string) (*amqp.Connection, error) {
+	conn, err := amqp.Dial(addr)
+
+	if err != nil {
+		return nil, RabbitError{util.WrapError(err, "unable to dial RabbitMQ address: %v", addr)}
+	}
+
+	client.changeConnection(conn)
+	client.logger.Println("Connected!")
+	return conn, nil
+}
+
+// ----------------------------------------------------------------------------
+
+// handleReconnect will wait for a channel error
+// and then continuously attempt to re-initialize both channels
+func (client *Client) handleReInit(conn *amqp.Connection) bool {
+	for {
+		client.isReady = false
+
+		err := client.init(conn)
+
+		if err != nil {
+			client.logger.Println("Failed to initialize channel. Retrying in", client.reInitDelay)
+
+			select {
+			case <-client.done:
+				return true
+			case <-time.After(client.reInitDelay):
+				client.reInitDelay = client.progressiveDelay(client.reInitDelay)
+			}
+			continue
+		}
+
+		// reset the init delay
+		client.reInitDelay = client.ReInitDelay
+
+		select {
+		case <-client.done:
+			return true
+		case <-client.notifyConnClose:
+			client.logger.Println("Connection closed. Reconnecting...")
+			return false
+		case <-client.notifyChanClose:
+			client.logger.Println("Channel closed. Re-running init...")
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------
+
+// init will initialize channel, declare the exchange, and declare the queue
+func (client *Client) init(conn *amqp.Connection) error {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Attempting to init RabbitMQ client while shutting down. Error:", r)
+		}
+	}()
+	ch, err := conn.Channel()
+
+	if err != nil {
+		return RabbitError{util.WrapError(err, "unable to initialize the RabbitMQ channel")}
+	}
+
+	err = ch.Confirm(false)
+
+	if err != nil {
+		return err
+	}
+	err = ch.ExchangeDeclare(
+		client.ExchangeName, // name
+		"direct",            // type
+		false,               // durable
+		false,               // auto-deleted
+		false,               // internal
+		false,               // no-wait
+		nil,                 // arguments
+	)
+	if err != nil {
+		return RabbitError{util.WrapError(err, "unable to declare the RabbitMQ exchange")}
+	}
+
+	var q amqp.Queue
+	q, err = ch.QueueDeclare(
+		client.QueueName, // name
+		false,            // durable
+		false,            // delete when unused
+		false,            // exclusive
+		false,            // no-wait
+		nil,              // arguments
+	)
+	if err != nil {
+		return RabbitError{util.WrapError(err, "unable to initialize the RabbitMQ queue")}
+	}
+
+	err = ch.QueueBind(
+		q.Name,              // queue name
+		client.RoutingKey,   // routing key
+		client.ExchangeName, // exchange
+		false,
+		nil,
+	)
+	if err != nil {
+		return RabbitError{util.WrapError(err, "unable to bind the RabbitMQ queue")}
+	}
+
+	client.changeChannel(ch)
+	client.isReady = true
+	client.notifyReady <- struct{}{}
+	client.logger.Println("Setup!")
+
+	return nil
+}
+
+// ----------------------------------------------------------------------------
+
+// changeConnection takes a new connection to the queue,
+// and updates the close listener to reflect this.
+func (client *Client) changeConnection(connection *amqp.Connection) {
+	client.connection = connection
+	client.notifyConnClose = make(chan *amqp.Error, 1)
+	client.connection.NotifyClose(client.notifyConnClose)
+}
+
+// ----------------------------------------------------------------------------
+
+// changeChannel takes a new channel to the queue,
+// and updates the channel listeners to reflect this.
+func (client *Client) changeChannel(channel *amqp.Channel) {
+	client.channel = channel
+	client.notifyChanClose = make(chan *amqp.Error, 1)
+	client.notifyConfirm = make(chan amqp.Confirmation, 1)
+	client.channel.NotifyClose(client.notifyChanClose)
+	client.channel.NotifyPublish(client.notifyConfirm)
+}
+
+// ----------------------------------------------------------------------------
+
+// progressively increase the retry delay
+func (client *Client) progressiveDelay(delay time.Duration) time.Duration {
+	return delay + time.Duration(rand.Intn(int(delay/time.Second)))*time.Second
+}
+
+// ----------------------------------------------------------------------------
+
+// Push will push data onto the queue and wait for a confirm.
+// If no confirm is received by the resendTimeout,
+// it re-sends messages until a confirm is received.
+// This will block until the server sends a confirm. Errors are
+// only returned if the push action itself fails, see UnsafePush.
+func (client *Client) Push(record queues.Record) error {
+
+	if !client.isReady {
+		// wait for client to be ready
+		<-client.notifyReady
+	}
+	for {
+		err := client.UnsafePush(record)
+		if err != nil {
+			client.logger.Println("Push failed. Retrying in", client.resendDelay, ". MessageId:", record.GetMessageId()) //TODO:  debug or trace logging, add messageId
+			select {
+			case <-client.done:
+				return errShutdown //TODO:  error message to include messageId?
+			case <-time.After(client.resendDelay):
+				client.resendDelay = client.progressiveDelay(client.resendDelay)
+			}
+			continue
+		}
+		select {
+		case confirm := <-client.notifyConfirm:
+			if confirm.Ack {
+				// reset resend delay
+				client.resendDelay = client.ResendDelay
+				return nil
+			}
+		case <-time.After(client.resendDelay):
+			client.resendDelay = client.progressiveDelay(client.resendDelay)
+		}
+		client.logger.Println("Push didn't confirm. Retrying in", client.resendDelay, ". MessageId:", record.GetMessageId()) //TODO:  debug or trace logging, add messageId
+	}
+}
+
+// ----------------------------------------------------------------------------
+
+// UnsafePush will push to the queue without checking for
+// confirmation. It returns an error if it fails to connect.
+// No guarantees are provided for if the server will
+// receive the message.
+func (client *Client) UnsafePush(record queues.Record) error {
+
+	if !client.isReady {
+		// wait for client to be ready
+		<-client.notifyReady
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	return client.channel.PublishWithContext(
+		ctx,                 // context
+		client.ExchangeName, // exchange name
+		client.RoutingKey,   // routing key
+		false,               // mandatory
+		false,               // immediate
+		amqp.Publishing{ // message
+			Body:         []byte(record.GetMessage()),
+			ContentType:  "text/plain",
+			DeliveryMode: amqp.Persistent,
+			MessageId:    record.GetMessageId(),
+		},
+	)
+}
+
+// ----------------------------------------------------------------------------
+
+// Consume will continuously put queue items on the channel.
+// It is required to call delivery.Ack when it has been
+// successfully processed, or delivery.Nack when it fails.
+// Ignoring this will cause data to build up on the server.
+//   - prefetch is the number of deliveries for the rabbit mq client to prefetch
+//     from the server.  a good rule of thumb is to prefetch as many as there
+//     will be clients processing the deliveries.
+func (client *Client) Consume(prefetch int) (<-chan amqp.Delivery, error) {
+	if !client.isReady {
+		// wait for client to be ready
+		<-client.notifyReady
+	}
+
+	if prefetch < 0 {
+		prefetch = 1
+	}
+
+	if err := client.channel.Qos(
+		prefetch, // prefetch count (should set to the number of load goroutines?)
+		0,        // prefetch size
+		false,    // global
+	); err != nil {
+		return nil, RabbitError{util.WrapError(err, "unable to set the RabbitMQ channel quality of service")}
+	}
+
+	return client.channel.Consume(
+		client.QueueName, // queue
+		"",               // consumer uid, blank auto-generates a uid
+		false,            // auto-ack
+		false,            // exclusive
+		false,            // no-local (not supported by rabbitmq)
+		false,            // no-wait, false seems safest
+		nil,              // args
+	)
+}
+
+// ----------------------------------------------------------------------------
+
+// Close will cleanly shutdown the channel and connection.
+func (client *Client) Close() error {
+	if !client.isReady {
+		return errAlreadyClosed
+	}
+	close(client.done)
+	close(client.notifyReady)
+
+	//FIXME:  connection.Close() closes underlying channels so do we need this?
+	err := client.channel.Close()
+	if err != nil {
+		client.logger.Println("channel close error:", err)
+	}
+
+	err = client.connection.Close()
+	if err != nil {
+		client.logger.Println("connection close error:", err)
+	}
+
+	client.isReady = false
+	return nil
+}
---
+++ queues/sqs/managedConsumer.go
@@ -0,0 +1,214 @@
+package sqs
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"runtime"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/roncewind/go-util/queues/sqs"
+	"github.com/roncewind/go-util/util"
+	"github.com/senzing/g2-sdk-go/g2api"
+	"github.com/senzing/go-common/record"
+	"github.com/sourcegraph/conc/pool"
+)
+
+var jobPool chan SQSJob
+
+type ManagedConsumerError struct {
+	error
+}
+
+// ----------------------------------------------------------------------------
+// Job implementation
+// ----------------------------------------------------------------------------
+
+// define a structure that will implement the Job interface
+type SQSJob struct {
+	client    *sqs.Client
+	engine    g2api.G2engine
+	id        int
+	message   types.Message
+	startTime time.Time
+	usedCount int
+	withInfo  bool
+}
+
+// ----------------------------------------------------------------------------
+
+// Job interface implementation:
+// Execute() is run once for each Job
+func (j *SQSJob) Execute(ctx context.Context, visibilitySeconds int32) error {
+	fmt.Println("DEBUG: start job execute. msg id:", *j.message.MessageId)
+	// increment the number of times this job struct was used and return to the pool
+	defer func() {
+		j.usedCount++
+		fmt.Println("DEBUG: end job execute. msg id:", *j.message.MessageId, "count:", j.usedCount)
+		jobPool <- *j
+	}()
+	// fmt.Printf("Received a message- msgId: %s, msgCnt: %d, ConsumerTag: %s\n", id, *j.message.MessageCount, *j.message.ConsumerTag)
+	record, newRecordErr := record.NewRecord(string(*j.message.Body))
+	if newRecordErr == nil {
+		fmt.Println("DEBUG: msg id:", *j.message.MessageId, "record id:", record.Id)
+		loadID := "Load"
+		//
+		visibilityContext, visibilityCancel := context.WithCancel(ctx)
+		go func() {
+			ticker := time.NewTicker(time.Duration(visibilitySeconds-1) * time.Second)
+			for {
+				select {
+				case <-ctx.Done():
+					j.client.SetMessageVisibility(visibilityContext, j.message, 0)
+					fmt.Println("DEBUG: job context cancelled")
+					return
+				case <-visibilityContext.Done():
+					fmt.Println("DEBUG: visibility context cancelled")
+					return
+				case <-ticker.C:
+					setVisibilityError := j.client.SetMessageVisibility(visibilityContext, j.message, visibilitySeconds)
+					if setVisibilityError != nil {
+						fmt.Println("ERROR: setting message visibility", setVisibilityError)
+						return
+					}
+					fmt.Println("DEBUG: record visibility extended", time.Since(j.startTime))
+				}
+			}
+		}()
+		if j.withInfo {
+			var flags int64 = 0
+			_, withInfoErr := j.engine.AddRecordWithInfo(ctx, record.DataSource, record.Id, record.Json, loadID, flags)
+			visibilityCancel()
+			if withInfoErr != nil {
+				fmt.Printf("Record in error: %s:%s:%s:%s\n", *j.message.MessageId, loadID, record.DataSource, record.Id)
+				return withInfoErr
+			}
+			//TODO:  what do we do with the "withInfo" data here?
+			// fmt.Printf("Record added: %s:%s:%s:%s\n", *j.message.MessageId, loadID, record.DataSource, record.Id)
+			// fmt.Printf("WithInfo: %s\n", withInfo)
+		} else {
+			addRecordErr := j.engine.AddRecord(ctx, record.DataSource, record.Id, record.Json, loadID)
+			visibilityCancel()
+			fmt.Println("Record added:", record.Id, "MessageId:", *j.message.MessageId)
+			if addRecordErr != nil {
+				fmt.Printf("ERROR: Add Record error: %s:%s:%s:%s\n", *j.message.MessageId, loadID, record.DataSource, record.Id)
+				return addRecordErr
+			}
+		}
+
+		// when we successfully process a message, delete it.
+		//as long as there was no error delete the message from the queue
+		err := j.client.RemoveMessage(ctx, j.message)
+		if err != nil {
+			fmt.Println("ERROR: Record not removed from queue. msg id:", *j.message.MessageId, "record:", record, "error:", err)
+		}
+		fmt.Println("DEBUG: Record removed from queue. msg id:", *j.message.MessageId)
+		fmt.Println("DEBUG:", *j.message.MessageId, "processing time", time.Since(j.startTime))
+	} else {
+		// logger.LogMessageFromError(MessageIdFormat, 2001, "create new szRecord", newRecordErr)
+		fmt.Println(time.Now(), "ERROR: Invalid delivery from SQS. msg id:", *j.message.MessageId)
+		// when we get an invalid delivery, send to the dead letter queue
+		err := j.client.PushDeadRecord(ctx, j.message)
+		if err != nil {
+			fmt.Println("ERROR: Unable to push message to the dead letter queue. msg id:", *j.message.MessageId, "record:", record, "error:", err)
+		}
+	}
+	return nil
+}
+
+// ----------------------------------------------------------------------------
+
+// Whenever Execute() returns an error or panics, this is called
+func (j *SQSJob) OnError(err error) {
+	fmt.Println("ERROR: Worker error:", err)
+	fmt.Println("ERROR: Failed to add record. msg id:", *j.message.MessageId)
+	err = j.client.PushDeadRecord(context.Background(), j.message)
+	if err != nil {
+		fmt.Println("ERROR: Pushing message to the dead letter queue. msg id:", *j.message.MessageId, "error:", err)
+	}
+}
+
+// ----------------------------------------------------------------------------
+// -- add records in SQS to Senzing
+// ----------------------------------------------------------------------------
+
+// Starts a number of workers that read Records from the given queue and add
+// them to Senzing.
+// - Workers restart when they are killed or die.
+// - respond to standard system signals.
+func StartManagedConsumer(ctx context.Context, urlString string, numberOfWorkers int, g2engine g2api.G2engine, withInfo bool, visibilitySeconds int32) error {
+
+	if g2engine == nil {
+		return errors.New("G2 Engine not set, unable to start the managed consumer")
+	}
+
+	//default to the max number of OS threads
+	if numberOfWorkers <= 0 {
+		numberOfWorkers = runtime.GOMAXPROCS(0)
+	}
+	fmt.Println(time.Now(), "Number of consumer workers:", numberOfWorkers)
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	client, err := sqs.NewClient(ctx, urlString)
+	if err != nil {
+		return ManagedConsumerError{util.WrapError(err, "unable to get a new SQS client")}
+	}
+	defer client.Close()
+
+	// setup jobs that will be used to process SQS deliveries
+	jobPool = make(chan SQSJob, numberOfWorkers)
+	for i := 0; i < numberOfWorkers; i++ {
+		jobPool <- SQSJob{
+			client:    client,
+			engine:    g2engine,
+			id:        i,
+			usedCount: 0,
+			withInfo:  withInfo,
+		}
+	}
+
+	messages, err := client.Consume(ctx, visibilitySeconds)
+	if err != nil {
+		fmt.Println(time.Now(), "Error getting delivery channel:", err)
+		return ManagedConsumerError{util.WrapError(err, "unable to get a new SQS message channel")}
+	}
+
+	p := pool.New().WithMaxGoroutines(numberOfWorkers)
+	jobCount := 0
+	// for message := range messages {
+	for message := range util.OrDone(ctx, messages) {
+		job := <-jobPool
+		job.message = message
+		job.startTime = time.Now()
+		fmt.Println("DEBUG: add to job queue. jobCount:", jobCount, "msg id:", *job.message.MessageId)
+		p.Go(func() {
+			err := job.Execute(ctx, visibilitySeconds)
+			if err != nil {
+				job.OnError(err)
+			}
+		})
+
+		jobCount++
+		if jobCount%10000 == 0 {
+			fmt.Println(time.Now(), "INFO: Jobs added to job queue:", jobCount)
+		}
+	}
+
+	// Wait for all the records in the record channel to be processed
+	p.Wait()
+
+	// clean up after ourselves
+	close(jobPool)
+	// drain the job pool
+	var job SQSJob
+	ok := true
+	for ok {
+		job, ok = <-jobPool
+		fmt.Println("Job:", job.id, "used:", job.usedCount)
+	}
+
+	return nil
+}
